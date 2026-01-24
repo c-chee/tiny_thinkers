b945c0111900fb3ad5140d7a54c39b0f5f639e6c
c-chee <christinechee2000@gmail.com>
2026-01-24 11:34:42 -0500

    minor merge fixes (#5)

--- server/src/server.js
+++ server/src/server.js
@@ -10,4 +10,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Tiny Thinkers server running on port ${PORT}`);
+  console.log(`Tiny Thinkers server running on port ${PORT}`); 
+});
