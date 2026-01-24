4c8111101017ce8f3da6e3dc00fbb951bdd4a530
c-chee <christinechee2000@gmail.com>
2026-01-24 11:46:52 -0500

    Database christinechee (#6)
    * minor merge fixes
    
    * uniform quotations

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -7,7 +7,7 @@ const userController = require('../controllers/user.controller');
 router.post('/signup', userController.signup);
 
 // POST /api/users/login
-router.post("/login", userController.login);
+router.post('/login', userController.login);
 
 module.exports = router;
 
