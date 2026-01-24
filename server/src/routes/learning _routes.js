19c188caaef00af8ebc8f887342ecb6dcac46506
Angel Martin <angelmartin@AngelAdatorsMBP.lan>
2026-01-24 13:31:09 -0500

    updated elementary api and comments

--- server/src/routes/learning _routes.js
+++ server/src/routes/learning _routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const { learnerDictionaryWord } = require('../services/learning_service');
 
+//learning dictionary
 router.get('/dictionary/:word', async (req, res) => {
     try {
         const {word} = req.params;
@@ -13,5 +14,17 @@ router.get('/dictionary/:word', async (req, res) => {
         res.status(500).json ({ error: 'Dictionary API failed'});
     }
 });
+
+//elementary dictionary
+router.get('/dictionary/elementary/:word', async (req, res) => {
+    try {
+        const {word} = req.params;
+        const data = await elementaryDictionaryWord(word);
+        res.json(data); 
+    } catch (err) {
+        res.status(500).json ({ error: 'Elementary API failed'});
+    }
+});
+
 
 module.exports = router;
--- server/src/services/learning_service.js
+++ server/src/services/learning_service.js
@@ -1,11 +1,8 @@
 const axios = require('axios');
 
+//learner api
 const LEARNER_API_KEY = process.env.MW_API_KEY;
 const LEARNER_API_URL = 'https://www.dictionaryapi.com/api/v3/references/learners/json';
-
-
-const ELEM_API_KEY = process.env.ELEM_MW_API_KEY;
-const ELEM_API_URL = 'https://www.dictionaryapi.com/api/v3/references/sd2/json';
 
 const learnerDictionaryWord = async (word) => {
     const response = await axios.get (`${LEARNER_API_URL}/${word}`,{
@@ -14,12 +11,18 @@ const learnerDictionaryWord = async (word) => {
     return response.data;
 };
 
+//elementary api
+const ELEM_API_KEY = process.env.ELEM_MW_API_KEY;
+const ELEM_API_URL = 'https://www.dictionaryapi.com/api/v3/references/sd2/json';
+
+
 const elementaryDictionaryWord = async (word) => {
     const response = await axios.get (`${ELEM_API_URL}/${word}`,{
         params: {key: ELEM_API_KEY}
     });
     return response.data;
 };
+
 
 module.exports = {
     learnerDictionaryWord, elementaryDictionaryWord
