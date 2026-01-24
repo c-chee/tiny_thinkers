1b749edbbf5ce368814c56cb651d5ca103cccc8e
Angel Martin <angelmartin@AngelAdatorsMBP.lan>
2026-01-24 11:51:44 -0500

    updated routes, learning service and APIs

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,20 @@
 const express = require('express');
-const path = require('path');
+
+const userRoutes = require('./routes/user_routes');
+const learningRoutes = require('./routes/learning _routes');
+
 const app = express();
+
+app.use(express.json());
+
+
+
+app.use('/api/learning', learningRoutes);
+
+app.use('/api/users', userRoutes);
+
+// app.use((req, res) => {
+//     res.status(404).json({ error: 'Route not found'});
+// });
 
 module.exports = app;
