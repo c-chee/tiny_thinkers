822c6c5793bc722b4196c7186c5bec3c99476258
Stephanie Pretell <wephiie@unknown7ae17ecfc9df.attlocal.net>
2026-01-30 08:40:47 -0500

    resolve merge conflicts before major edits -steph

--- server/src/server.js
+++ server/src/server.js
@@ -3,17 +3,13 @@
  * - This file's only job is to turn the server on.
  * - Like the power button to a pc
  */
-<<<<<<< HEAD
-require('dotenv').config(); // Loads env variables
-=======
-require('dotenv').config();
->>>>>>> 0b6f26e659f08f6fe59f956ee1bb2c3588a7991f
+require("dotenv").config();
 
-const app = require('./app');
+const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 
 // Starts the server
 app.listen(PORT, () => {
-  console.log(`Tiny Thinkers server running on port ${PORT}`); 
+  console.log(`Tiny Thinkers server running on port ${PORT}`);
 });
