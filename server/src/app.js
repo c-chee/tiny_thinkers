9a6118ebaf0ae9581ccb969ad2bdfaabea25bdda
Zayra Guerrero Camarillo <zayraadilenecamarillo@gmail.com>
2026-02-01 09:22:56 -0500

    login - Zayra Guerrero (#37)
    * files for login
    
    * login and registration
    
    * Front end brendan (#25)
    
    * revamped home page with styling and cards
    
    * mobile slideshow for stat cards with changed color and updated styling per discussion
    
    ---------
    
    Co-authored-by: Brendan Jefferson <brendanjefferson@macbookpro.attlocal.net>
    Co-authored-by: c-chee <christinechee2000@gmail.com>
    
    * Letter cards (#26)
    
    * finished cards layout and interactions -steph
    
    * updated card files -steph
    
    ---------
    
    Co-authored-by: Stephanie Pretell <wephiie@Mac.attlocal.net>
    
    * login
    
    * resources
    
    * Added animated login/signup page and styling. Updated nav links
    
    * Pushing up updated login/sign up files
    
    ---------
    
    Co-authored-by: Zayra Guerrero Camarillo <zayraadielencamarillo@gmail.com>
    Co-authored-by: c-chee <christinechee2000@gmail.com>
    Co-authored-by: Brendan J <82038297+bjefferz@users.noreply.github.com>
    Co-authored-by: Brendan Jefferson <brendanjefferson@macbookpro.attlocal.net>
    Co-authored-by: Stephanie Pretell <wephiie@Mac.attlocal.net>

--- server/src/app.js
+++ server/src/app.js
@@ -127,14 +127,21 @@ app.get("/Login", (req, res) => {
 });
 
 
-// === RESOURCE ===
-// app.get('/resources', (req, res) => {
-//   res.render('resources', {
-//     layout: 'resourcelayout',
-//     title: 'Resources',
-//     categories: resourcesData.categories,
-//   });
-// });
+
+// === VOLUNTEER ===
+app.get('/volunteer', (req, res) => {
+    res.render('volunteer', {
+      layout: 'volunteerlayout',
+      title: 'Volunteer'
+    });
+});
+// === Resources ===
+app.get('/resources', (req, res) => {
+    res.render('resources', {
+      layout: 'resourceslayout',
+      title: 'Resources'
+    });
+})
 
 // === 404 HANDLER === 
 // *** Must be last ***
