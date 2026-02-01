de40136b9c3bad9eb0f31c8c035b18184e633273
Christine Chee <christinechee2000@gmail.com>
2026-02-01 18:24:14 -0500

    dash routes

--- server/src/controllers/dashboard.controller.js
+++ server/src/controllers/dashboard.controller.js
@@ -1,19 +1,54 @@
+// const db = require("../db");
+
+// exports.getDashboard = async (req, res) => {
+//     try {
+//         // TEMP fake user until auth exists
+//         const userId = 1;
+
+//         const [prefs] = await db.query(
+//         "SELECT * FROM user_preferences WHERE user_id = ?",
+//         [userId]
+//         );
+
+//         let contentTypes = ["reading", "dictionary", "spelling", "cards"];
+
+//         if (prefs.length && prefs[0].content_type !== "all") {
+//         contentTypes = prefs[0].content_type.split(",");
+//         }
+
+//         res.render("dashboard", {
+//         layout: "dashboard-layout",
+//         pageTitle: "Dashboard",
+//         contentTypes
+//         });
+
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send("Dashboard error");
+//     }
+// };
+
+// controllers/dashboard.controller.js
 const db = require("../db");
+
+// List of all possible content types
+const allContentTypes = ["reading", "alphabet", "vocab", "spelling", "dictionary"];
 
 exports.getDashboard = async (req, res) => {
     try {
-        // TEMP fake user until auth exists
+        // TEMP user until auth is ready
         const userId = 1;
 
-        const [prefs] = await db.query(
+        // Fetch user preferences
+        const [prefsRows] = await db.query(
         "SELECT * FROM user_preferences WHERE user_id = ?",
         [userId]
         );
 
-        let contentTypes = ["reading", "dictionary", "spelling", "cards"];
+        let contentTypes = allContentTypes;
 
-        if (prefs.length && prefs[0].content_type !== "all") {
-        contentTypes = prefs[0].content_type.split(",");
+        if (prefsRows.length && prefsRows[0].content_type !== "all") {
+        contentTypes = prefsRows[0].content_type.split(",");
         }
 
         res.render("dashboard", {
