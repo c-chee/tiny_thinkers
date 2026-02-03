4e5817e43b53d76b841a62f1f03d6b80eb93a5d4
Zayra Guerrero Camarillo <zayraadilenecamarillo@gmail.com>
2026-02-03 14:52:48 -0500

    Zayra reading (#73)
    * adding volunteer form w/o submit page
    
    * Volunteer Styling Updated
    
    * Styling Volunteer
    
    * Add volunteer form thank you message
    
    * Starting Raading Comp Styling
    
    ---------
    
    Co-authored-by: Zayra Guerrero Camarillo <zayraadielencamarillo@gmail.com>
    Co-authored-by: Christine Chee <christinechee2000@gmail.com>

--- client/public/js/comp.js
+++ client/public/js/comp.js
@@ -1,6 +1,11 @@
-const inputs = document.querySelectorAll('.day-tracker input');
+document.addEventListener('DOMContentLoaded', () => {
 
+    // READIING DAY TRACKER
+
+const inputs = document.querySelectorAll('.day-tracker input');
 const totalMinutesEl = document.getElementById('totalMinutes');
+const titleEL = document.querySelector('#typingTitle')
+const text = titleEL.textContent;
 
 function updateTotal(){
     let total = 0;
@@ -21,3 +26,21 @@ inputs.forEach((input, index) => {
 });
 
 updateTotal();
+
+// TYPING EFFECT 
+titleEL.textContent = "";
+
+let index = 0;
+const speed = 200;
+
+function type() {
+    if (index < text.length) {
+        titleEL.textContent += text.charAt(index);
+        index++
+        setTimeout(type, speed);
+    }
+}
+
+type();
+
+});
